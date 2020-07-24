8643b7718ba499a7ed541cba4466b3e8952d8842
Timothy Saudners <timsaunders88@gmail.com>
2020-07-24 16:16:29 +0100

    added test for function add_receipt_item in bartab class

--- tests/test_bartab.py
+++ tests/test_bartab.py
@@ -1,14 +1,20 @@
 from classes.bartab import Bartab
+from classes.guest import Guest
 import unittest
 
 class TestBartab(unittest.TestCase):
     def setUp(self):
         self.tab = Bartab()
+        self.guest = Guest("Tim", 32, 20.5)
 
     def test_bartab_init(self):
         self.assertEqual(0, self.tab.total)
-        self.assertEqual(list, type(self.tab.history))
+        self.assertEqual(list, type(self.tab.receipt))
 
     def test_bartab_increase(self):
         self.tab.increase_total(10)
         self.assertEqual(10, self.tab.total)
+
+    def test_add_receipt_item(self):
+        self.tab.add_receipt_item(self.guest, "Entry Fee", 10)
+        self.assertEqual([{"Customer":"Tim", "Sale Item":"Entry Fee", "Value":10}], self.tab.receipt)
