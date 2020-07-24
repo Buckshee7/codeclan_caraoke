def99c48c097665d65ed3765ff840f049d80568a
Timothy Saudners <timsaunders88@gmail.com>
2020-07-24 16:04:26 +0100

    added test for bartab class increase_total function

--- tests/test_bartab.py
+++ tests/test_bartab.py
@@ -8,3 +8,6 @@ class TestBartab(unittest.TestCase):
     def test_bartab_init(self):
         self.assertEqual(0, self.tab.total)
         self.assertEqual(list, type(self.tab.history))
+
+    def test_bartab_increase(self):
+        self.assertEqual(10, self.tab.increase_total(10))
