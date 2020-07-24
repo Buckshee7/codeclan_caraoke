1a423e02be280b565206810180ddf4d0982dc061
Timothy Saudners <timsaunders88@gmail.com>
2020-07-24 18:14:38 +0100

    added test for guest class for presence of fav_song property

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -1,10 +1,12 @@
 from classes.guest import Guest
+from classes.song import Song
 import unittest
 
 class TestGuest(unittest.TestCase):
     
     def setUp(self):
         self.guest = Guest("Tim", 32, 20.5)
+        self.song = Song("Don't Stop Believin'", "Journey")
 
     def test_guest_has_name(self):
         self.assertEqual("Tim", self.guest.name)
@@ -14,6 +16,9 @@ class TestGuest(unittest.TestCase):
 
     def test_guest_has_wallet(self):
         self.assertEqual(20.5, self.guest.wallet)
+
+    def test_guest_has_fav_song_if_provided(self):
+        self.assertEqual(self.song, self.guest.fav_song)
 
     def test_guest_wallet_reduction(self):
         self.guest.reduce_wallet(5)
