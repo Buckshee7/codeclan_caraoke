e78c9168cd6e15c40dd88895a0a944d92b176e99
Timothy Saudners <timsaunders88@gmail.com>
2020-07-24 14:39:33 +0100

    added tests for room class properties number and capacity

--- tests/room_test.py
+++ tests/room_test.py
@@ -2,4 +2,11 @@ from classes.room import Room
 import unittest
 
 class TestRoom(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.room = Room(1, 5)
+
+    def test_room_has_number(self):
+        self.assertEqual(1, self.room.number)
+
+    def test_room_has_capacity(self):
+        self.assertEqual(5, self.room.capacity)
