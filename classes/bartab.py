0292068d9438e6e48c133a0851fe877ad9ef180a
Timothy Saudners <timsaunders88@gmail.com>
2020-07-24 16:17:09 +0100

    added add_receipt_item to bartab class

--- classes/bartab.py
+++ classes/bartab.py
@@ -1,7 +1,11 @@
 class Bartab:
     def __init__(self):
         self.total = 0
-        self.history = []
+        self.receipt = []
 
     def increase_total(self, amount):
         self.total += amount
+
+    def add_receipt_item(self, guest, reason, amount):
+        item = {"Customer":guest.name, "Sale Item":reason, "Value":amount}
+        self.receipt.append(item)
