e46684a39ab9265f51ed8a8fd1973e734a48d927
Timothy Saudners <timsaunders88@gmail.com>
2020-07-24 14:31:38 +0100

    adds tests for guest properties age and name

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -2,4 +2,12 @@ from classes.guest import Guest
 import unittest
 
 class TestGuest(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.guest = Guest("Tim", 32)
+
+    def test_guest_has_name(self):
+        self.assertEqual("Tim", self.guest.name)
+
+    def test_guest_has_age(self):
+        self.assertEqual(32, self.guest.age)
